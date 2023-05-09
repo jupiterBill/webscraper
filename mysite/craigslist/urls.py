3c31c646f470147b6733f82bb1423ede455f954c
jupiterBill <techwizardhyped@gmail.com>
2023-05-09 20:53:06 +0100

    front end almost done

--- mysite/craigslist/urls.py
+++ mysite/craigslist/urls.py
@@ -2,4 +2,5 @@ from . import  views
 from django.urls import path
 urlpatterns = [
  path("",views.index, name = "index"),
+ path("new_search", views.new_search, name = "new_search")
 ]
--- mysite/craigslist/views.py
+++ mysite/craigslist/views.py
@@ -1,4 +1,14 @@
+import requests
 from django.shortcuts import render
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
 def index(request):
     return render(request, template_name= 'base.html')
+def new_search(request):
+    search = request.POST["search"]
+    context = {
+        "search": search
+    }
+    print(search)
+    return render(request, "craigslist/new_search.html",context)
 # Create your views here.
